420c29a5fb6cc08d55683ceaae105f356336b9cb
Keks <keks@htmlacademy.ru>
2022-11-05 13:52:02 +0300

    :heavy_check_mark: Сборка #3

--- 3/js/main.js
+++ 3/js/main.js
@@ -1,2 +1,5 @@
-const takeNumber = (begin, end) => (begin >= 0 && end >= begin) ? Math.floor(Math.random()) *(begin - end + 1) + end : 'Ошибка';
-console.log(takeNumber(1, 6));
+const takeNumber = (min, max) => (min >= 0 && max >= min) ? Math.floor(Math.random()) *(min - max + 1) + max : 'Ошибка';
+(takeNumber(1, 6));
+
+const checkMaxLength =(string, maxLength) => string.length < maxLength;
+(checkMaxLength('Кекс', 7));
