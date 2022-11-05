59c716f394dbd4bd69f8df720fc34f568b0981e9
Мария Бердышева <maria.berdysheva@urfu.me>
2022-11-05 15:49:40 +0500

    example

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-const takeNumber = (min, max) => (min >= 0 && nax >= min) ? Math.floor(Math.random()) *(min - max + 1) + max : 'Ошибка';
-console.log(takeNumber(1, 6));
+const takeNumber = (min, max) => (min >= 0 && max >= min) ? Math.floor(Math.random()) *(min - max + 1) + max : 'Ошибка';
+(takeNumber(1, 6));
 
 const checkMaxLength =(string, maxLength) => string.length < maxLength;
-console.log(checkMaxLength("Кекс", 7));
+(checkMaxLength('Кекс', 7));
