6fb54e5501ea3d189e4be78ce8ae293e15aeebd4
Мария Бердышева <maria.berdysheva@urfu.me>
2022-12-19 10:57:11 +0500

    Модуляция

---
+++ js/picture.js
@@ -0,0 +1,23 @@
+import { createPhotos } from "./data";
+
+const pictureTemplate = document.querySelector('#picture').content;
+
+const picturesData = creatPhotoDescriptions();
+
+const picturesContainer = document.querySelector('.pictures');
+
+const pictureFragment = document.createDocumentFragment();
+
+const getPictureElement = (url, likes, comments) => {
+  const pictureElement = pictureTemplate.cloneNode(true);
+  pictureElement.querySelector('.picture__img').src = url;
+  pictureElement.querySelector('.picture__likes').textContent = likes;
+  pictureElement.querySelector('.picture__comments').textContent = comments.length;
+  pictureFragment.appendChild(pictureElement);
+};
+
+picturesData.forEach(({url, likes, comments}) => {
+  getPictureElement(url, likes, comments);
+});
+
+picturesContainer.appendChild(pictureFragment);
